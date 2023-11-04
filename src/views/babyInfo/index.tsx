690e21039650396af763efe5692b6da23edb164c
dennys.aguilar <dennyz114@gmail.com>
2023-11-03 19:46:37 -0500

    - fixing days calculation - fixing baby info styles

--- src/views/babyInfo/index.tsx
+++ src/views/babyInfo/index.tsx
@@ -3,19 +3,23 @@ import {
   BABY_BIRTHDAY,
   BABY_BORN_HEIGHT,
   BABY_BORN_WEIGHT,
-  BABY_LAST_NAME,
   BABY_MIDDLE_NAME,
   BABY_NAME
 } from '../../utils/constants'
 import './BabyInfo.scss'
 import BabyImage from '../../images/Alice.jpeg'
-import { getTimeSinceBornUntilNow } from '../../utils/dateUtils'
+import { getStringDateFullFormat, getTimeSinceBornUntilNow } from '../../utils/dateUtils'
+import { FaWeightHanging } from 'react-icons/fa'
+import { GiBodyHeight } from 'react-icons/gi'
+import { MdDateRange } from 'react-icons/md'
+
+const getAge = () => {
+  const [years, months, days] = getTimeSinceBornUntilNow(new Date(BABY_BIRTHDAY))
+  const yearsToShow = years > 0 ? `${years} año${years > 1 ? 's, ' : ', '}` : ''
+  return `${yearsToShow} ${months} mes${months > 1 ? 'es' : ''} y ${days} dia${days > 1 ? 's' : ''}`
+}
 
 const BabyInfo = () => {
-  const now = new Date()
-  const [years, months, days, hours, minutes] = getTimeSinceBornUntilNow(new Date(BABY_BIRTHDAY))
-
-
   return (
     <div className={'baby-profile-info'}>
       <h2 className={'baby-profile-name'}>Mis datos</h2>
@@ -23,14 +27,23 @@ const BabyInfo = () => {
         <img src={BabyImage} alt="baby-image"/>
       </div>
       <div className={'baby-profile-data'}>
-        <div className={'baby-born-data'}>
-          <p>{`Me llamo ${BABY_NAME} ${BABY_MIDDLE_NAME} ${BABY_LAST_NAME}`}</p>
+        <div className={'baby-name'}>
+          <p>{BABY_NAME} {BABY_MIDDLE_NAME}</p>
         </div>
         <div className={'baby-born-data'}>
-          <p>{`Nací pesando ${BABY_BORN_WEIGHT}Kg. y midiendo ${BABY_BORN_HEIGHT}m.`}</p>
+          <p>Datos de nacimiento:</p>
+          <div className={'baby-info'}>
+            <FaWeightHanging/> {BABY_BORN_WEIGHT}Kg.
+          </div>
+          <div className={'baby-info'}>
+            <GiBodyHeight/> {BABY_BORN_HEIGHT}m.
+          </div>
+          <div className={'baby-info'}>
+            <MdDateRange/> {getStringDateFullFormat(BABY_BIRTHDAY)}
+          </div>
         </div>
-        <div className={'baby-born-data'}>
-          <p>{`Hoy tengo ${months} mes ${days} dias ${hours} horas y ${minutes} minutos`}</p>
+        <div className={'baby-data'}>
+          <p>Hoy tengo {getAge()}</p>
         </div>
       </div>
     </div>
